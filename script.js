bc14ceb847a05262fce731aff2e75199d44c62a9
BeWithSadique <mdsadiquealam590@gmail.com>
2024-11-08 02:18:26 +0530

    add css

---
+++ script.js
@@ -0,0 +1,4 @@
+const navdailog = document.getElementById('nav-dailog')
+function handleMenu(){
+    navdailog.classList.toggle('hidden')
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#ff5783',
+        'sadique':'#03e17c ',
+      },
+      fontFamily:{
+        'display': ['Poppins','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
